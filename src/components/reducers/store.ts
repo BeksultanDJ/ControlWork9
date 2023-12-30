bd69c06efb899c666dec929e4e487de0cd680311
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 22:09:44 +0600

    добавил второй слайс

--- src/components/reducers/store.ts
+++ src/components/reducers/store.ts
@@ -1,9 +1,12 @@
+// Пример подключения в вашем Redux Store
 import { configureStore } from '@reduxjs/toolkit';
 import transactionReducer from './transactionSlice';
+import categoryReducer from "./categorySlice";
 
 export const store = configureStore({
     reducer: {
         transactions: transactionReducer,
+        categories: categoryReducer,
     },
 });
 
