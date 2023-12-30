3ac8cb1b8bbebd838381a2cacfbb53d73625d649
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 22:10:06 +0600

    удалил запросы с категориями

--- src/components/reducers/transactionSlice.ts
+++ src/components/reducers/transactionSlice.ts
@@ -7,11 +7,6 @@ interface Transaction {
     id: string;
 }
 
-interface Category {
-    name:string;
-    type: string;
-    id: string;
-}
 
 export const sendTransactionData = createAsyncThunk(
     'transactions/sendTransactionData',
@@ -31,31 +26,6 @@ export const sendTransactionData = createAsyncThunk(
 
             const data = await response.json();
             console.log('Transaction added:', data);
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
-    }
-);
-
-export const sendCategoryData = createAsyncThunk(
-    'categories/sendCategoryData',
-    async (categoryData: Category, { rejectWithValue }) => {
-        try {
-            const response = await fetch('https://controll-17843-default-rtdb.europe-west1.firebasedatabase.app/categories.json', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(categoryData),
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const data = await response.json();
-            console.log('Category added:', data);
             return data;
         } catch (error) {
             return rejectWithValue(error.message);
