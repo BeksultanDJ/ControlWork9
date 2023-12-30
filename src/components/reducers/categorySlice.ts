56266670ce1030aba8f7e336b17087585315add6
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 22:55:31 +0600

    добавил запрос на удаления добавил редюсер для удаления

--- src/components/reducers/categorySlice.ts
+++ src/components/reducers/categorySlice.ts
@@ -46,6 +46,28 @@ export const getCategory = createAsyncThunk(
     }
 );
 
+export const deleteCategoryData = createAsyncThunk(
+    'categories/deleteCategoryData',
+    async (categoryId: string, { rejectWithValue }) => {
+        try {
+            const response = await fetch(`https://controll-17843-default-rtdb.europe-west1.firebasedatabase.app/categories/${categoryId}.json`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            return categoryId;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
 interface CategoryState {
     categories: Category[];
 }
@@ -62,9 +84,14 @@ const categorySlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(getCategory.fulfilled, (state, action) => {
             state.categories = Object.values(action.payload);
-        });
+        }),
+            builder.addCase(deleteCategoryData.fulfilled, (state, action) => {
+                console.log('Category deleted from server with ID:', action.payload);
+                state.categories = state.categories.filter(category => category.id !== action.payload);
+            });
     },
 });
+
 
 export const categoryActions = categorySlice.actions;
 export default categorySlice.reducer;
