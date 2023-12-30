6e4033309e266463928eb61975516ca760ade2f2
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 17:46:39 +0600

    создал коспонент который отправляет пост запрос по указаному апи

---
+++ src/components/reducers/transactionActions.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const sendTransactionData = createAsyncThunk(
+    'transactions/sendTransactionData',
+    async (transactionData, { rejectWithValue }) => {
+        try {
+            const response = await fetch('https://controll-17843-default-rtdb.europe-west1.firebasedatabase.app/transactions.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(transactionData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log('Transaction added:', data);
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
