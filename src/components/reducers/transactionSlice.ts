cb1095da6b9cd23f6d3c74d192bfa1de8ed60836
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 21:34:47 +0600

    добавил интерфейс для категорий и апи запрос для категорий

--- src/components/reducers/transactionSlice.ts
+++ src/components/reducers/transactionSlice.ts
@@ -7,6 +7,12 @@ interface Transaction {
     id: string;
 }
 
+interface Category {
+    name:string;
+    type: string;
+    id: string;
+}
+
 export const sendTransactionData = createAsyncThunk(
     'transactions/sendTransactionData',
     async (transactionData: Transaction, { rejectWithValue }) => {
@@ -31,6 +37,32 @@ export const sendTransactionData = createAsyncThunk(
         }
     }
 );
+
+export const sendCategoryData = createAsyncThunk(
+    'categories/sendCategoryData',
+    async (categoryData: Category, { rejectWithValue }) => {
+        try {
+            const response = await fetch('https://controll-17843-default-rtdb.europe-west1.firebasedatabase.app/categories.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(categoryData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log('Category added:', data);
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
 
 export const getTransactions = createAsyncThunk(
     'transactions/getTransactions',
