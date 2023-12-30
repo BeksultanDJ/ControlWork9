61b719bbbac194380539e989a03e14993b967c07
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 16:12:17 +0600

    создал компонент отображающий все расходы

---
+++ src/components/DisplayInfo.tsx
@@ -0,0 +1,17 @@
+
+
+const DisplayInfo = () => {
+    return (
+        <div className="card">
+            <div>
+                <p>Date</p>
+                <p>Category</p>
+                <strong>Sum</strong>
+            </div>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default DisplayInfo;
