f22ef0b74d9bec6659c5f8aaa361476d2bb1e4a3
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 19:31:51 +0600

    импортировал слайс и функцию отправки данных указал в каком виде данные должеы отправлятся и сделал форму отправки

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
-import AddExIn from "./components/Add.tsx";
 import DisplayInfo from "./components/DisplayInfo.tsx";
 import Categories from "./components/Categories.tsx";
 
@@ -8,7 +7,6 @@ function App() {
 
   return (
     <>
-      <AddExIn/>
         <DisplayInfo/>
         <Routes>
             <Route path="categories" element={<Categories/>}/>
