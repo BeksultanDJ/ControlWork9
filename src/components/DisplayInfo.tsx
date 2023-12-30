0db4a94b56d5d74da517f06e0a411caf7897d033
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-12-30 19:25:19 +0600

    импортировл слайс и сделал отобрадения созданных расходов так же сделал на проверку наличия их

--- src/components/DisplayInfo.tsx
+++ src/components/DisplayInfo.tsx
@@ -1,17 +1,45 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTransactions } from './reducers/transactionSlice';
 
+const DisplayInfo: React.FC = () => {
+    const dispatch = useDispatch();
+    const transactions = useSelector((state: any) => state.transactions.transactions);
 
-const DisplayInfo = () => {
+    useEffect(() => {
+        dispatch(getTransactions());
+    }, [dispatch]);
+
     return (
-        <div className="card">
-            <div>
-                <p>Date</p>
-                <p>Category</p>
-                <strong>Sum</strong>
+        <>
+            <header>
+                <div className="container">
+                    <h2>Finance Tracker</h2>
+                    <div className="Links">
+                        <strong>Categories</strong>
+                        <strong>Add</strong>
+                    </div>
+                </div>
+            </header>
+            <div className="cardsContainer">
+                {Array.isArray(transactions) && transactions.length > 0 ? (
+                    transactions.map((transaction, index) => (
+                        <div className="card" key={index}>
+                            <div className="cardsInfo">
+                                <p>Date: {transaction.createdAt}</p>
+                                <p>Category: {transaction.category}</p>
+                                <strong>Sum: {transaction.amount}</strong>
+                            </div>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </div>
+                    ))
+                ) : (
+                    <p>No transactions available</p>
+                )}
             </div>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
-    )
-}
+        </>
+    );
+};
 
 export default DisplayInfo;
